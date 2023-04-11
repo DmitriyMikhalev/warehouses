ccde3a2ab1da0fd324a0b5d947ced218e0d6f6ad
Dmitriy Mikhalev <fakeforall@mail.ru>
2023-04-11 14:34:13 +0500

    Refactor view functions at warehouse app -- move logic into 1 function

--- warehouses/warehouse/views.py
+++ warehouses/warehouse/views.py
@@ -6,6 +6,113 @@ from .forms import (QueryDateForm, QueryFullnameForm, QuerySixForm,
                     QueryWarehouseNameForm)
 
 
+QUERY_INFO = {
+    1: {
+        'columns': (
+            'id',
+            'first_name',
+            'email'
+        ),
+        'description': settings.QUERY_1_DESCRIPTION,
+        'form_class': None,
+        'sql_cmd': 'SELECT * FROM query_1() ORDER BY id ASC;',
+    },
+    2: {
+        'columns': (
+            'id',
+            'brand',
+            'max_capacity'
+        ),
+        'description': settings.QUERY_2_DESCRIPTION,
+        'form_class': None,
+        'sql_cmd': 'SELECT * FROM query_2() ORDER BY id ASC;',
+    },
+    3: {
+        'columns': (
+            'id',
+            'name',
+            'address',
+            'max_capacity'
+        ),
+        'description': settings.QUERY_3_DESCRIPTION,
+        'form_class': None,
+        'sql_cmd': 'SELECT * FROM query_3() ORDER BY id ASC;',
+    },
+    4: {
+        'columns': (
+            'id',
+            'name',
+            'address',
+            'date_start with TZ',
+            'date_end with TZ'
+        ),
+        'description': settings.QUERY_4_DESCRIPTION,
+        'form_class': QueryDateForm,
+        'sql_cmd': 'SELECT * FROM query_4(\'{}\') ORDER BY id ASC;',
+    },
+    5: {
+        'columns': (
+            'id',
+            'first_name',
+            'last_name',
+            'total_payload'
+        ),
+        'description': settings.QUERY_5_DESCRIPTION,
+        'form_class': QueryFullnameForm,
+        'sql_cmd': 'SELECT * FROM query_5(\'{}\', \'{}\') ORDER BY id ASC;',
+    },
+    6: {
+        'columns': (
+            'id',
+            'address',
+            'date with TZ'
+        ),
+        'description': settings.QUERY_6_DESCRIPTION,
+        'form_class': QuerySixForm,
+        'sql_cmd': 'SELECT * FROM query_6(\'{}\', \'{}\', \'{}\');',
+    },
+    7: {
+        'columns': (
+            'id',
+            'brand',
+            'max_capacity',
+            'date_start with TZ'
+        ),
+        'description': settings.QUERY_7_DESCRIPTION,
+        'form_class': QueryDateForm,
+        'sql_cmd': 'SELECT * FROM query_7(\'{}\');',
+    },
+    8: {
+        'columns': (
+            'id',
+            'address'
+        ),
+        'description': settings.QUERY_8_DESCRIPTION,
+        'form_class': QueryDateForm,
+        'sql_cmd': 'SELECT * FROM query_8(\'{}\') ORDER BY id;',
+    },
+    9: {
+        'columns': (
+            'count',
+        ),
+        'description': settings.QUERY_9_DESCRIPTION,
+        'form_class': QueryDateForm,
+        'sql_cmd': 'SELECT * FROM query_9(\'{}\');',
+    },
+    10: {
+        'columns': (
+            'id',
+            'name',
+            'article_number',
+            'tonnage'
+        ),
+        'description': settings.QUERY_10_DESCRIPTION,
+        'form_class': QueryWarehouseNameForm,
+        'sql_cmd': 'SELECT * FROM query_10(\'{}\', \'{}\');',
+    },
+}
+
+
 def index(request):
     return render(
         request=request,
@@ -13,261 +120,30 @@ def index(request):
     )
 
 
-def query_1(request):
+def query_view(request, query_index):
     template_name = 'warehouse/index.html'
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM query_1() ORDER BY id ASC;')
-        data = cursor.fetchall()
+    params = QUERY_INFO.get(query_index)
+    sql_cmd = params.get('sql_cmd')
+    form_args = []
     context = {
-        'description': settings.QUERY_1_DESCRIPTION,
-        'data': data,
-        'columns': ('id', 'first_name', 'email')
-    }
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_2(request):
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM query_2() ORDER BY id ASC;')
-        data = cursor.fetchall()
-
-    context = {
-        'description': settings.QUERY_2_DESCRIPTION,
-        'data': data,
-        'columns': ('id', 'brand', 'max_capacity')
-    }
-
-    return render(
-        request=request,
-        template_name='warehouse/index.html',
-        context=context
-    )
-
-
-def query_3(request):
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM query_3() ORDER BY id ASC;')
-        data = cursor.fetchall()
-
-    context = {
-        'description': settings.QUERY_3_DESCRIPTION,
-        'data': data,
-        'columns': ('id', 'name', 'address', 'max_capacity')
-    }
-
-    return render(
-        request=request,
-        template_name='warehouse/index.html',
-        context=context
-    )
-
-
-def query_4(request):
-    template_name = 'warehouse/index.html'
-    form = QueryDateForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_4_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'name', 'address', 'date_start with TZ', 'date_end with TZ')
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
+        key: val for key, val in params.items() if key in (
+            'columns', 'description'
         )
-
-    with connection.cursor() as cursor:
-        arg = str(form.cleaned_data.get('date'))
-        cursor.execute(f'SELECT * FROM query_4(\'{arg}\') ORDER BY id ASC;')
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_5(request):
-    template_name = 'warehouse/index.html'
-    form = QueryFullnameForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_5_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'first_name', 'last_name', 'total_payload')
     }
 
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
-
-    with connection.cursor() as cursor:
-        first_name = form.cleaned_data.get('first_name')
-        last_name = form.cleaned_data.get('last_name')
-        cursor.execute(
-            'SELECT * FROM query_5(\'{}\', \'{}\') ORDER BY id ASC;'.format(
-                first_name, last_name
+    if (form_class := params.get('form_class')) is not None:
+        form = form_class(request.POST or None)
+        context['form'] = form
+        if not form.is_valid():
+            return render(
+                request=request,
+                template_name=template_name,
+                context=context
             )
-        )
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_6(request):
-    template_name = 'warehouse/index.html'
-    form = QuerySixForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_6_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'address', 'date with TZ')
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
+        form_args = form.cleaned_data.values()
 
     with connection.cursor() as cursor:
-        first_name = form.cleaned_data.get('first_name')
-        last_name = form.cleaned_data.get('last_name')
-        date = form.cleaned_data.get('date')
-        cursor.execute(
-            'SELECT * FROM query_6(\'{}\', \'{}\', \'{}\');'.format(
-                first_name, last_name, date
-            )
-        )
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_7(request):
-    template_name = 'warehouse/index.html'
-    form = QueryDateForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_7_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'brand', 'max_capacity', 'date_start with TZ')
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
-
-    with connection.cursor() as cursor:
-        arg = form.cleaned_data.get('date')
-        cursor.execute(f'SELECT * FROM query_7(\'{arg}\');')
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_8(request):
-    template_name = 'warehouse/index.html'
-    form = QueryDateForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_8_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'address')
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
-
-    with connection.cursor() as cursor:
-        arg = form.cleaned_data.get('date')
-        cursor.execute(f'SELECT * FROM query_8(\'{arg}\') ORDER BY id;')
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_9(request):
-    template_name = 'warehouse/index.html'
-    form = QueryDateForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_9_DESCRIPTION,
-        'form': form,
-        'columns': ('count',)
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
-
-    with connection.cursor() as cursor:
-        arg = form.cleaned_data.get('date')
-        cursor.execute(f'SELECT * FROM query_9(\'{arg}\');')
-        context['data'] = cursor.fetchall()
-
-    return render(
-        request=request,
-        template_name=template_name,
-        context=context
-    )
-
-
-def query_10(request):
-    template_name = 'warehouse/index.html'
-    form = QueryWarehouseNameForm(request.POST or None)
-    context = {
-        'description': settings.QUERY_10_DESCRIPTION,
-        'form': form,
-        'columns': ('id', 'name', 'article_number', 'tonnage')
-    }
-
-    if not form.is_valid():
-        return render(
-            request=request,
-            template_name=template_name,
-            context=context
-        )
-
-    with connection.cursor() as cursor:
-        name = form.cleaned_data.get('name')
-        address = form.cleaned_data.get('address')
-        cursor.execute(
-            'SELECT * FROM query_10(\'{}\', \'{}\');'.format(name, address)
-        )
+        cursor.execute(sql_cmd.format(*form_args))
         context['data'] = cursor.fetchall()
 
     return render(
